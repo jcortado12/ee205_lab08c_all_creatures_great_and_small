6fcfdf30bc7dc95bd62d9c37949f37c98d5910ca
jcortado <jcortado@hawaii.edu>
2022-03-10 11:55:41 -1000

    Initial Operational Version

--- Animal.h
+++ Animal.h
@@ -26,9 +26,9 @@ public:
 
     Animal(const std::string &species, Gender gender);
 
-    Animal(const std::string &species, Gender gender, float weight);
+    Animal(const std::string &species, Gender gender, float newWeight);
 
-    Animal(const std::string &species, float weight);
+    Animal(const std::string &species, float newWeight);
 
     static const std::string &getKingdom();
 
@@ -36,21 +36,28 @@ public:
 
     Gender getGender() const;
 
-    void setGender(Gender gender);
+    void setGender(Gender newGender);
 
     float getWeight() const;
 
-    void setWeight(float weight);
+    void setWeight(float newWeight);
 
     void printInfo();
 
     bool isValid();
 
-private:
+protected:
     static const std::string    kingdom;
     std::string                 species;                    //Required
     enum Gender                 gender  = UNKNOWN_GENDER;   //Defaulting to UNKNOWN_GENDER
     float                       weight  = UNKNOWN_WEIGHT;   //Defaulting to UNKNOWN_WEIGHT
+
+    bool validateWeight( const float newWeight );
+    bool validateSpecies( const std::string newSpecies);
+
+private:
+    void setSpecies( const std::string newSpecies );
+
 };
 
 
